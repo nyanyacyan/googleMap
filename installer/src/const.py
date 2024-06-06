e7cf4891305d445166273714159667b0dcb2723f
nyanyacyan <nyanyacyan@gmail.com>
2024-06-07 05:11:29 +0900

    constファイルを追加

---
+++ installer/src/const.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+endpoint_url ="https://maps.googleapis.com/maps/api/place/textsearch/json"
+
+place_details_endpoint_url = "https://maps.googleapis.com/maps/api/place/details/json"
+
+geocoding_endpoint_url = "https://maps.googleapis.com/maps/api/geocode/json"
